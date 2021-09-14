25de506ed45633adec10b7c7ddc7fc087d975c30
yonoyo <90102917+yonoyo@users.noreply.github.com>
2021-09-14 22:48:25 +0800

    Update sanziqi.c

--- sanziqi.c
+++ sanziqi.c
@@ -7,110 +7,110 @@
 #include<time.h>
 
 void menu(){
-	printf("*********************\n");
-	printf("1.play        0.exit\n");
-	printf("*********************\n");
+    printf("*********************\n");
+    printf("1.play        0.exit\n");
+    printf("*********************\n");
 }
 
 
 void Intnboard(char arr[ROW][COL], int row, int col){
-	int i = 0, j = 0;
+    int i = 0, j = 0;
 
-	for (i = 0; i < row; i++)
-		for (j = 0; j < col; j++)
-			arr[i][j] = ' ';
+    for (i = 0; i < row; i++)
+        for (j = 0; j < col; j++)
+            arr[i][j] = ' ';
 }
 void display(char arr[ROW][COL], int row, int col){
-	int i = 0, j = 0;
-	for (i = 0; i < row; i++){
-		printf(" %c | %c | %c \n", arr[i][0], arr[i][1], arr[i][2]);
-		if (i < row - 1)
-			printf("---|---|---\n");
-	}
+    int i = 0, j = 0;
+    for (i = 0; i < row; i++){
+        printf(" %c | %c | %c \n", arr[i][0], arr[i][1], arr[i][2]);
+        if (i < row - 1)
+            printf("---|---|---\n");
+    }
 }
 
 void player(char arr[ROW][COL], int row, int col){
-	int x = 0, y = 0;
-	printf("Íæ¼Ò×ß£º\n");
-	while (1)
-	{
-		printf("ÇëÊäÈëÒª×ßµÄÎ»ÖÃ:");
-		scanf("%d%d", &x, &y);
-		if (x >= 1 && x <= row  &&  y >= 1 && y <= col)
-		{
-			if (arr[x - 1][y - 1] = ' ')
-			{
-				arr[x - 1][y - 1] = '*';
-				break;
-			}
+    int x = 0, y = 0;
+    printf("Ã•ÃŠÂºâ€œâ—Šï¬‚Â£âˆ«\n");
+    while (1)
+    {
+        printf("Â«ÃŽÂ â€°Â»ÃŽâ€œâ„¢â—Šï¬‚ÂµÆ’Å’ÂªÃ·âˆš:");
+        scanf("%d%d", &x, &y);
+        if (x >= 1 && x <= row  &&  y >= 1 && y <= col)
+        {
+            if (arr[x - 1][y - 1] = ' ')
+            {
+                arr[x - 1][y - 1] = '*';
+                break;
+            }
 
-			else
-			{
+            else
+            {
 
-				printf("¸ÃÎ»ÖÃ±»Õ¼Áì\n");
-			}
+                printf("âˆâˆšÅ’ÂªÃ·âˆšÂ±Âªâ€™ÂºÂ¡Ã\n");
+            }
 
-		}
-		else
-		{
-			printf("ÊäÈë·Ç·¨£¬ÇëÖØÐÂÊäÈë");
-		}
-	}
+        }
+        else
+        {
+            printf("Â â€°Â»ÃŽâˆ‘Â«âˆ‘Â®Â£Â¨Â«ÃŽÃ·Ã¿â€“Â¬Â â€°Â»ÃŽ");
+        }
+    }
 }
 
 void computer(char arr[ROW][COL], int row, int col){
-	int x = 0, y = 0;
-	printf("µçÄÔ£º\n");
-	while (1){
-		x = rand() % row;
-		y = rand() % col;
-		if (arr[x][y] = ' '){
-			arr[x][y] = '#';
-			break;
-		}
+    int x = 0, y = 0;
+    printf("ÂµÃÆ’â€˜Â£âˆ«\n");
+    while (1){
+        x = rand() % row;
+        y = rand() % col;
+        if (arr[x][y] ==' '){
+            arr[x][y] = '#';
+            break;
+        }
 
-	}
+    }
 
 }
 
 int isfull(char arr[ROW][COL], int row, int col){
-	int i = 0, j = 0;
-	for (i = 0; i < row; i++){
-		for (j = 0; j < col; j++){
-			if (arr[i][j] = ' ')
-				return 1;
-		}
+    int i = 0, j = 0;
+    for (i = 0; i < row; i++){
+        for (j = 0; j < col; j++){
+            if (arr[i][j] == ' ')
+                return 0;
+        }
 
-	}
-	return 0;
+    }
+  return 1;
 }
 
 
 char boolw(char arr[ROW][COL], int row, int col){
-	for (int i = 0; i < row; i++){
-		if (arr[i][0] == arr[i][1] == arr[i][2] && arr[i][0] != ' ')
-			return arr[i][0];
-	}
+    for (int i = 0; i < row; i++){
+        if (arr[i][0] == arr[i][1] == arr[i][2] && arr[i][0] != ' ')
+            return arr[i][0];
+    }
 
 
-	for (int i = 0; i < row; i++){
-		if (arr[0][i] == arr[1][i] == arr[2][i] && arr[0][i] != ' ')
-			return arr[0][0];
-	}
+    for (int i = 0; i < row; i++){
+        if (arr[0][i] == arr[1][i] == arr[2][i] && arr[0][i] != ' ')
+            return arr[0][0];
+    }
 
 
 
-	if (arr[0][0] == arr[1][1] == arr[2][2] && arr[0][0] != ' ')
-		return arr[1][1];
+    if (arr[0][0] == arr[1][1] == arr[2][2] && arr[0][0] != ' ')
+        return arr[1][1];
 
-	if (arr[2][0] == arr[1][1] == arr[2][2] && arr[0][2] != ' ')
-		return arr[1][1];
+    if (arr[2][0] == arr[1][1] == arr[2][2] && arr[0][2] != ' ')
+        return arr[1][1];
 
-	if (1 == isfull(arr, ROW, COL)){
-		return 'C';
-	}
-	else
-		return 'Q';
+    if (1 == isfull(arr, ROW, COL)){
+        return 'C';
+    }
+    else
+        return 'Q';
 
 
 
@@ -119,54 +119,54 @@ char boolw(char arr[ROW][COL], int row, int col){
 
 
 void play(){
-	int ret = 0;
-	char arr[ROW][COL] = { 0 };
-	Intnboard(arr, ROW, COL);
-	display(arr, ROW, COL);
-	while (1){
-		player(arr, ROW, COL);
-		display(arr, ROW, COL);
-		ret=boolw(arr, ROW, COL);
-		if (ret != 'C')
-			break;
-		computer(arr,ROW,COL);
-		display(arr, ROW, COL);
-		ret=boolw(arr, ROW, COL);
-		if (ret != 'C')
-			break;
-	}
-	if (ret == '*')
-		printf("Íæ¼ÒÓ®\n");
-	else if (ret == '#')
-		printf("µçÄÔÓ®\n");
-	else
-		printf("Æ½¾Ö\n");
-	
-	
+    int ret = 0;
+    char arr[ROW][COL] = { 0 };
+    Intnboard(arr, ROW, COL);
+    display(arr, ROW, COL);
+    while (1){
+        player(arr, ROW, COL);
+        display(arr, ROW, COL);
+        ret=boolw(arr, ROW, COL);
+        if (ret == 'C')
+            break;
+        computer(arr,ROW,COL);
+        display(arr, ROW, COL);
+        ret=boolw(arr, ROW, COL);
+        if (ret == 'C')
+            break;
+    }
+    if (ret == '*')
+        printf("Ã•ÃŠÂºâ€œâ€Ã†\n");
+    else if (ret == '#')
+        printf("ÂµÃÆ’â€˜â€Ã†\n");
+    else
+        printf("âˆ†Î©Ã¦Ã·\n");
+    
+    
 }
-	
+    
 
 
 int main(){
-	srand((unsigned int)time(NULL));
-	menu();
-	int input = 0;
-	do{
-		printf("ÇëÑ¡Ôñ£º\n");
-		scanf("%d", &input);
-		switch (input){
-		case 1:
-			play();
-			break;
-		case 0:
-			printf("ÍË³ö\n");
-			break;
-		default:
-			printf("Ñ¡Ôñ´íÎó£¬ÇëÖØÐÂÊäÈë£¡£¡£¡\n");
-			break;
-		}
-		}while (input);
-		system("pause");
-		return 0;
-	}
-	
+    srand((unsigned int)time(NULL));
+    menu();
+    int input = 0;
+    do{
+        printf("Â«ÃŽâ€”Â°â€˜Ã’Â£âˆ«\n");
+        scanf("%d", &input);
+        switch (input){
+        case 1:
+            play();
+            break;
+        case 0:
+            printf("Ã•Ã€â‰¥Ë†\n");
+            break;
+        default:
+            printf("â€”Â°â€˜Ã’Â¥ÃŒÅ’Ã›Â£Â¨Â«ÃŽÃ·Ã¿â€“Â¬Â â€°Â»ÃŽÂ£Â°Â£Â°Â£Â°\n");
+            break;
+        }
+        }while (input);
+        system("pause");
+        return 0;
+    }
+    
